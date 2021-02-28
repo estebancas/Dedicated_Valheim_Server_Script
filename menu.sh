7c25c0be40ec4a543265c252cf71997ee771d1da
Zerobandwidth <nimdavirus@gmail.com>
2021-02-28 15:41:38 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -396,8 +396,8 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
-confDisplayName=$displayname
-confValheimPassword="$password
+confDisplayName="'${displayname}'"
+confValheimPassword=$password
 
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
