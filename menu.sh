dbf11be4d9b09a6fd937ad8d67c69caaec3b9f25
Zerobandwidth <nimdavirus@gmail.com>
2021-02-28 15:25:29 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -401,7 +401,7 @@ confValheimPassword="${password}"
 
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name ${confDisplayName} -port 2456 -nographics -batchmode -world "Thorsunderwear" -password ${confValheimPassword}
+./valheim_server.x86_64 -name $confDisplayName -port 2456 -nographics -batchmode -world "Thorsunderwear" -password $confValheimPassword
 
 #export LD_LIBRARY_PATH=
 export LD_LIBRARY_PATH=$templdpath
