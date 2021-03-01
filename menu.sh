f974f3eda59db87478064dd5fa3d9468656dab23
Zerobandwidth <nimdavirus@gmail.com>
2021-03-01 22:30:50 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -466,8 +466,8 @@ sleep 2
 clear
 tput setaf 2; echo "Check server status by typing systemctl status valheimserver.service"
 tput setaf 2; echo "Thank you for using the script."
-tput setaf 2; echo "AND A HUGE THANKS TO github: @RedKrieg, @bherbruck, @xaviablaza, @joaoanes"
-tput setaf 2; echo "@amasover, @madmozg, @nicolas-martin, @devdavi, @Lachlanmac and others!"
+tput setaf 2; echo "AND A HUGE THANKS TO github: @Lachlanmac, @JamieeLee, @RedKrieg, @bherbruck "
+tput setaf 2; echo "@xaviablaza, @joaoanes, @amasover, @madmozg, @nicolas-martin, @devdavi and others!"
 tput setaf 2; echo "If your name is missing! Let me know!"
 tput setaf 2; echo "Twitch: ZeroBandwidth"
 tput setaf 2; echo "GLHF"
